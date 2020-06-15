6ead034c9d691120529ea65069666bd393a658ee
Alia <alia@Alias-MacBook-Pro.local>
2020-06-16 00:35:47 +0300

    solved calculator task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 // Define the numbers and the operation here
-
+const x = 8;
+const y = 2;
+const operator = "+";
 // Write if-statements here
+if (operator === "+") {
+  console.log(x + y);
+} else if (operator === "/") {
+  console.log(x / y);
+} else if (operator === "-") {
+  console.log(x - y);
+} else if (operator === "*") {
+  console.log(x * y);
+}
